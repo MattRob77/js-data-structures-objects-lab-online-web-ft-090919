a01fbf41708a81af5cfa133b97b501afdfcaa82b
Matthew Robinson <matt_robinson1@yahoo.com>
2019-12-17 18:33:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
     let newDriver = {...driver}
-    newDriver[key] = value
+    newDriver[key] = value; 
     return newDriver
 }
+
+
 
