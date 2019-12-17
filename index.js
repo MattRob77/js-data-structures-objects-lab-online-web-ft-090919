b743d9161d46c3738587f21b65300339c9e293fd
Matthew Robinson <matt_robinson1@yahoo.com>
2019-12-17 18:35:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) { 
-  let newDriver = Object.assign({})
+  let newDriver = Object.assign({}, driver)
   
 } 
