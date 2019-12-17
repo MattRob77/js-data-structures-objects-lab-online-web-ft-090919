2308209ab0799f6796bb70016cbfa9d3b0c49be2
Matthew Robinson <matt_robinson1@yahoo.com>
2019-12-17 18:32:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Write your solution in this file!
 let driver = {} 
 
-function updateDriverWithKeyAndValue(driver, key, value) { 
-  let newdriver = {...driver} 
-  newDriver[key] = value 
-  return newDriver 
-  
-} 
+function updateDriverWithKeyAndValue(driver, key, value) {
+    let newDriver = {...driver}
+    newDriver[key] = value
+    return newDriver
+}
 
